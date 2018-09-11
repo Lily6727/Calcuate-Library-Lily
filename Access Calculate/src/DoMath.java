425e45129f78c764203cc8862c52bb9e5a9bb6b2
APCS3rd <APCS3rd@MHS-Dreyer-06>
2018-09-11 11:45:07 -0700

    almost done the part1

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,20 @@
+//This class contains methods to  perform various mathematical topic
+//@author LilyChen
+//@ September,6th,2018
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(Calculate.square(5));
+       System.out.println(Calculate.cube(5));
+       System.out.println(Calculate.average(5,6));
+       System.out.println(Calculate.overloading(5,6,7));
+       System.out.println(Calculate.toDegrees(10*3.14159));
+       System.out.println(Calculate.toRadians(30));
+       System.out.println(Calculate.discriminant(1,2,1));
+       System.out.println(Calculate.toImproperFrac(3,1,2));
+       
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,46 @@
+//This class contains methods that perform various operations
+//@author LilyChen
+// September 6,2018
+
+public class Calculate {
+	//return the square to input
+	public static int square(int number) {
+		int answer;
+		answer = number*number;
+       return answer;
+       //return number*number;
+	}
+	public static int cube(int number) {
+		int answer;
+		answer = number*number*number;
+		return answer;
+		//return number*number*number;
+	}
+	public static double average(double number1,double number2) {
+		double answer = 0.5*(number1 + number2);
+		return answer;
+	}
+	public static double overloading(double number1,double number2, double number3) {
+		double answer = (number1 + number2 + number3)/3;
+		return answer;
+	}
+	public static double toDegrees(double number) {
+		double answer = 180/3.14159*(number);
+		return answer;
+	}
+	public static double toRadians(double number) {
+		double answer = 3.14159/180*(number);
+		return answer;
+	}
+	public static double discriminant(double a,double b,double c) {
+		double answer = (b*b-4*a*c)/2/a;
+		return answer;
+	}
+	public static String toImproperFrac(int a, int b, int c) {
+		String answer = (a*c + b)+"/" + c;
+		return answer;
+	}
+    public static String toMixedNum(int a, int b) {
+    	String answer = (a/b +"_" + (a-))
+    }
+}
